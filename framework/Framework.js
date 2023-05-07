307930a34decc8d7da11ab16742bbcecc6457910
Максим Рулетов <maksim.manezkiy.evg.99@gmail.com>
2023-05-07 12:30:36 +0300

    0.4.8-alpha
    Исправлена ошибка с регулярными выражениями

--- framework/Framework.js
+++ framework/Framework.js
@@ -29,7 +29,7 @@ const Meta = {
      * @readonly
      * @type {string}
      */
-    version: '0.4.8-alpha',
+    version: '0.4.9-alpha',
     /**
      * Официальным и единственных распространителем UndevEngine и всех его
      * компонентов является UndevSoftware. Любые модификации фреймворка не
