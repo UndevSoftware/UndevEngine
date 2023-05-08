f5bd30909368f4d9fe2b39c96af5c97239012812
Максим Рулетов <maksim.manezkiy.evg.99@gmail.com>
2023-05-08 14:20:43 +0300

    1.5.10-alpha
    Добавлена папка с плагинам, начата разработка WYSIWYG редактора

---
+++ Plugins/Editor.js
@@ -0,0 +1,21 @@
+/**
+ * Текстовый редактор для сайта.
+ * 
+ * @public
+ * @author Максим Рулетов (ruletkasuperstar)
+ * @license GPLv3
+ */
+
+/**
+ * Главный класс для редактора.
+ * 
+ * @public
+ * @author Максим Рулетов (ruletkasuperstar)
+ * @param {Element}
+ */
+function Editor(
+    /* Родительский элемент, к которому будет пристроен редактор */
+    element
+) {
+    
+};
