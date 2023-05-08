57554d4af36f4913c2b986ef0a533e0aec0f1e6b
Максим Рулетов <maksim.manezkiy.evg.99@gmail.com>
2023-05-08 14:36:05 +0300

    1.6.12-alpha
    Исправлены мелкие ошибки и добавлено чтение элемента

--- Plugins/Editor.js
+++ Plugins/Editor.js
@@ -11,11 +11,34 @@
  * 
  * @public
  * @author Максим Рулетов (ruletkasuperstar)
- * @param {Element}
+ * @param {Element} element
  */
 function Editor(
     /* Родительский элемент, к которому будет пристроен редактор */
     element
 ) {
-    
+    /**
+     * Родительский элемент.
+     * 
+     * @type {Element}
+     */
+    this.element;
+
+    if (!element || !element instanceof Element) {
+        if (document.querySelector('#root') == null) {
+            const ROOT = document.createElement('div');
+                ROOT.id = "root";
+
+            document.body.append(ROOT);
+        }
+
+        this.element = document.querySelector(ROOT);
+    }
+    else {
+        if (document.body.contains(element) == null) {
+            document.body.append(element);
+        }
+
+        this.element = element;
+    }
 };
