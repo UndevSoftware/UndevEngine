900efb4f820e7ac2dfd61840b790aed95e4b3bf6
Максим Рулетов <maksim.manezkiy.evg.99@gmail.com>
2023-05-11 07:42:22 +0300

    2.19.94-alpha
    Добавлен новый демо-проект для тестирования плагинов

---
+++ demo/DEMO_02.js
@@ -0,0 +1,33 @@
+/* Подключение фреймворка */
+const { Framework } = require('../framework/Framework.js');
+const { Test } = require('../Plugins/Test/Test.js');
+/* Подключение файловой системы */
+const fs = require('fs');
+
+/* Создание фреймворка */
+const FRWK = new Framework('dev');
+/* Инициализация сервера */
+const SRVR = new FRWK.Server('192.168.1.102', 3000);
+
+/* Интеграция плагина */
+FRWK.Intagrate(Test);
+
+/* Установление ключа SSL сертификата */
+SRVR.Set('key', fs.readFileSync('./demo/certs/selfsigned.key'));
+/* Установление SSL сертификата */
+SRVR.Set('cert', fs.readFileSync('./demo/certs/selfsigned.crt'));
+
+/* Создание сервеа */
+SRVR.Instantiate();
+
+/* Обработка GET-маршрута */
+SRVR.Get('/', (request, response) => {
+    response.render('/demo/UI/main.ehtml', {
+        test: 'tested'
+    });
+
+    response.end();
+});
+
+/* Запуск сервера */
+SRVR.Listen();
