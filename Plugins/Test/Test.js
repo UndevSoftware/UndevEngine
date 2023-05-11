6a0872f95199afbe43a89d80477494610150589e
Максим Рулетов <maksim.manezkiy.evg.99@gmail.com>
2023-05-11 07:40:47 +0300

    2.19.94-alpha
    папка плагина Test перенесена в /Plugins

---
+++ Plugins/Test/Test.js
@@ -0,0 +1,16 @@
+const Test = {
+	"Data": [],
+	"Templates": [],
+	"Themes": [],
+	"Styles": [
+		{
+			"selector": ".test",
+			"styles": {
+				"color": "black",
+				"background": "white"
+			}
+		}
+	]
+}
+
+exports.Test = Test;
